6d37260906a99ae82680eea2a62b46517852cae5
1170022060 <38129225+1170022060@users.noreply.github.com>
2026-03-20 19:11:53 +0800

    Update live_header.js
    11

--- live_header.js
+++ live_header.js
@@ -1,4 +1,9 @@
-const headers = $request.headers || {};
+let headers = $request.headers || {};
+
 headers["Referer"] = "https://d.f2d6.co/#/live/512/";
+headers["referer"] = "https://d.f2d6.co/#/live/512/";
 headers["User-Agent"] = "Mozilla/5.0";
+headers["user-agent"] = "Mozilla/5.0";
+
+console.log("QX header injected => " + $request.url);
 $done({ headers });
